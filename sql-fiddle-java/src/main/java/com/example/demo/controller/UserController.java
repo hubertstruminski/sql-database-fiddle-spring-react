5901867b536aaa63c500c6a9d0dedb7a93ffb779
hubert.struminski <hubert.struminski@gmail.com>
2019-05-30 17:24:34 +0200

    add JwtLoginSuccessResponse class to payload package and create login mapping in UserController class

--- sql-fiddle-java/src/main/java/com/example/demo/controller/UserController.java
+++ sql-fiddle-java/src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.User;
+import com.example.demo.payload.JwtLoginSuccessResponse;
 import com.example.demo.security.JwtTokenProvider;
 import com.example.demo.service.UserService;
 import com.example.demo.service.MapErrorValidator;
@@ -9,12 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+
+import static com.example.demo.security.SecurityConstants.TOKEN_PREFIX;
 
 @Controller
 public class UserController {
@@ -33,6 +39,24 @@ public class UserController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody UserRegisterValidator userValidator, BindingResult result) {
+        ResponseEntity<?> errorMap = mapErrorValidator.validateToMap(result);
+
+        if(errorMap != null) {
+            return errorMap;
+        }
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userValidator.getUserName(), userValidator.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JwtLoginSuccessResponse(true, jwt));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody UserRegisterValidator userValidator, BindingResult result) {
