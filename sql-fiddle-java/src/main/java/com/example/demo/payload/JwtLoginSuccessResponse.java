ad6f7e2fdef22253c2fcdcac0f6cf76b497609ca
hubert.struminski <hubert.struminski@gmail.com>
2019-11-27 20:59:06 +0100

    add getters/setters for JwtLoginSuccessResponse payload class

--- sql-fiddle-java/src/main/java/com/example/demo/payload/JwtLoginSuccessResponse.java
+++ sql-fiddle-java/src/main/java/com/example/demo/payload/JwtLoginSuccessResponse.java
@@ -17,4 +17,20 @@ public class JwtLoginSuccessResponse {
                 ", token='" + token + '\'' +
                 '}';
     }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
