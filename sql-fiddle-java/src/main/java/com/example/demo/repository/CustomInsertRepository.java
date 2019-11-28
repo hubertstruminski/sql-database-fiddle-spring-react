fbc5e7db51ca133a54f44bb061453d1625eeaa02
hubert.struminski <hubert.struminski@gmail.com>
2019-11-28 16:34:34 +0100

    initial commit after deploy on heroku

--- sql-fiddle-java/src/main/java/com/example/demo/repository/CustomInsertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.CustomInsert;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomInsertRepository extends CrudRepository<CustomInsert, Long> {
-}
--- sql-react-js/src/components/Layout/MessageSuccess.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class MessageSuccess extends React.Component {
-    render() {
-        return <div className="alert alert-success">User has been created successfully</div>
-    }
-}
-
-export default MessageSuccess;
